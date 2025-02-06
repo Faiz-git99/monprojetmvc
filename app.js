2e725310d00454fde58ee07dba518b1d6d4f84e1
Faiz-git99 <marifaiz97@gmail.com>
2025-02-06 10:06:52 +0300

    creation de route post

--- app.js
+++ app.js
@@ -80,5 +80,47 @@ app.get("/formulaireProgrammeTv", (req, res) => {
     res.render("formulaireProgrammeTv")
 });
 
+// crée une route avec post pour ajouter un élément dans la base de données
+app.post("/modification", (req, res) => {
+    console.log("corps requête body", req.body);
+    console.log("corps requête chainetv", req.body.chainetv);
+    console.log("corps requête num_chainetv", req.body.num_chainetv);
+    console.log("corps requête descriptif", req.body.descriptif);
+
+    let ID = req.body.id || null;  // If no id is provided, set it to null
+    let chainetv = req.body.chainetv;
+    let num_chainetv = req.body.num_chainetv;
+    let descriptif = req.body.descriptif;
+    let requeteSQL;
+    let donnees;
+
+    if (!ID) {
+        // If no ID (new record), insert
+        requeteSQL = "INSERT INTO programmediffusion (chainetv, num_chainetv, descriptif) VALUES (?, ?, ?)";
+        donnees = [chainetv, num_chainetv, descriptif];
+    } else {
+        // If there is an ID (update existing record)
+        requeteSQL = "UPDATE programmediffusion SET chainetv = ?, num_chainetv = ?, descriptif = ? WHERE id = ?";
+        donnees = [chainetv, num_chainetv, descriptif, ID];
+    }
+
+    req.getConnection((erreur, connection) => {
+        if (erreur) {
+            console.log(erreur);
+            res.status(500).send("Database connection error");
+        } else {
+            connection.query(requeteSQL, donnees, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send("Error executing SQL query");
+                } else {
+                    console.log("Modification réussi");
+                    res.status(302).redirect("/programmeTv");  // Redirect after insertion or update
+                }
+            });
+        }
+    });
+});
+
 
 module.exports = app;
