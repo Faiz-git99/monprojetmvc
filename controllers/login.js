874e7ec22806d6af940cf602c5179a5fc0adc40c
Faiz-git99 <marifaiz97@gmail.com>
2025-02-16 08:21:31 +0300

    inscription reussie à la base de données et connexion réussie

--- controllers/login.js
+++ controllers/login.js
@@ -2,5 +2,38 @@
 module.exports = {
     loginView: (req, res) => {
         res.render('login')
+    },
+
+validationlogin: async (req, res) => {
+    console.log("corps requête email", req.body.email);
+    console.log("corps requête mot de passe", req.body.mdp);
+
+    const email = req.body.email;
+    const mdp = req.body.mdp;
+    let requeteSQL = "SELECT * FROM utilisateur WHERE email = ?";
+    
+    
+    if(!email || !mdp) {
+        return res.send('login', {error : "Veuillez compléter tous les champs."});
     }
+ 
+        req.getConnection((Erreur, connection) => {
+            if(Erreur) {
+                console.log("erreur");
+                return res.send("Vous n'êtes pas arrivé à la base de donnée");
+            } else {
+                connection.query(requeteSQL, [email, mdp], (err, connexion) => {
+                    if(err) {
+                        console.log("erreur de connexion à votre compte", err)
+                    } else {
+                        console.log("Je me suis connécté à mon compte", connexion);
+
+                        
+                        res.status(302).redirect("/programmeTv");
+                    }
+                })
+            }
+        })
+    }
+
 }
--- controllers/signup.js
+++ controllers/signup.js
@@ -2,5 +2,44 @@
 module.exports = {
     signupView: (req, res) => {
         res.render('signup')
+    },
+
+    validationsignup : async (req, res) => {
+        console.log("corps requête body", req.body);
+        console.log("corps requête nom", req.body.identifiantnom);
+        console.log("corps requête prénom", req.body.identifiantprenom);
+        console.log("corps requête de l'email", req.body.identifiantmail);
+        console.log("corps requête date de naissance", req.body.identifiantnaissance);
+        console.log("corps requête mot de passe", req.body.mdp);
+    
+         
+        let nom = req.body.identifiantnom;
+        let prenom = req.body.identifiantprenom;
+        let email = req.body.identifiantmail;
+        let naissance = req.body.identifiantnaissance;
+        let mdp = req.body.mdp;
+        let requeteSQL;
+        let donnees;
+    
+        
+        
+        requeteSQL = "INSERT INTO utilisateur (id, nom, prenom, email, mot_de_passe, date_de_naissance) VALUES (?, ?, ?, ?, ?, ? )";
+        donnees = [null, nom, prenom, email, mdp, naissance];
+    
+        req.getConnection((erreur, connection) => {
+            if (erreur) {
+                console.log("erreur");
+            } else {
+                
+                connection.query(requeteSQL, donnees, (err, ajout) => {
+                    if (err) {
+                        console.log("erreur de de connexion", err);
+                    } else {
+                        console.log("mission réussi");
+                        res.status(302).redirect("/signup");
+                    }
+                });
+            }
+        });
     }
 }
