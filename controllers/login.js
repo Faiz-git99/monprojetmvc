9ae32c7a64113ff8affa180f4d098b333d686fc2
Faiz-git99 <marifaiz97@gmail.com>
2025-02-26 09:29:44 +0300

    importation de la fonction bcrypt sur le code validationlogin

--- controllers/login.js
+++ controllers/login.js
@@ -1,39 +1,63 @@
+// Importation de bcrypt pour le hachage du mot de passe
+const bcrypt = require('bcrypt');
+
+
 // exporte de fichier login
 module.exports = {
     loginView: (req, res) => {
         res.render('login')
     },
 
-validationlogin: async (req, res) => {
-    console.log("corps requête email", req.body.email);
-    console.log("corps requête mot de passe", req.body.mdp);
+// exporte la fonction validationlogin de la route login par la méthode POST
+    validationlogin: async (req, res) => {
+        console.log("corps requête email", req.body.email);
+        console.log("corps requête mot de passe", req.body.mdp);
 
-    const email = req.body.email;
-    const mdp = req.body.mdp;
-    let requeteSQL = "SELECT * FROM utilisateur WHERE email = ?";
+        const email = req.body.email;
+        const mdp = req.body.mdp;
+
+        let requeteSQL = "SELECT * FROM utilisateur WHERE email = ?";
+
+            req.getConnection((Erreur, connection) => {
+                if(Erreur) {
+                    console.log("erreur");
+                    return res.send("Vous n'êtes pas arrivé à la base de donnée");
+                } 
+
+                    connection.query(requeteSQL, [email], async (err, results) => {
+                        if(err) {
+                            console.log("erreur de connexion à votre compte", err)
+                            return res.send("erreur de connexion");
+                        } 
+
+                    // verifier si l'utilisateur existe
+                        if (results.length === 0) {
+                            console.log("Utilisateur non trouvé");
+                            return res.send("Identifiant incorrect");
+                        }
+                    
+                        const user = results[0];
+
+                    // recupérer le mot de passe hache de la base de données
+                        const hashedPassword = user.mot_de_passe;
+
+                    
+                    // comparer le mot de passe en clair avec le mot de passe haché
+                        const verifyPassword = await bcrypt.compare(mdp, hashedPassword); 
+
+                            if (verifyPassword) {
+                                console.log("connexion réussie");
+                                res.status(302).redirect("/programmeTv");
+                            } else {
+                                console.log("mot de passe incorrect");
+                                res.status(302).send("le mot de passe saisi est incorrect");
+                                }
+                    
+                    });
+                
+            
+            });
+    } 
     
-    
-    if(!email || !mdp) {
-        return res.send('login', {error : "Veuillez compléter tous les champs."});
-    }
- 
-        req.getConnection((Erreur, connection) => {
-            if(Erreur) {
-                console.log("erreur");
-                return res.send("Vous n'êtes pas arrivé à la base de donnée");
-            } else {
-                connection.query(requeteSQL, [email, mdp], (err, connexion) => {
-                    if(err) {
-                        console.log("erreur de connexion à votre compte", err)
-                    } else {
-                        console.log("Je me suis connécté à mon compte", connexion);
 
-                        
-                        res.status(302).redirect("/programmeTv");
-                    }
-                })
-            }
-        })
-    }
-
-}
+};
--- controllers/signup.js
+++ controllers/signup.js
@@ -1,3 +1,7 @@
+// Importation de bcrypt pour le hachage du mot de passe
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 // exporte de fichier signup
 module.exports = {
     signupView: (req, res) => {
@@ -5,41 +9,48 @@ module.exports = {
     },
 
     validationsignup : async (req, res) => {
-        console.log("corps requête body", req.body);
-        console.log("corps requête nom", req.body.identifiantnom);
-        console.log("corps requête prénom", req.body.identifiantprenom);
-        console.log("corps requête de l'email", req.body.identifiantmail);
-        console.log("corps requête date de naissance", req.body.identifiantnaissance);
-        console.log("corps requête mot de passe", req.body.mdp);
-    
-         
-        let nom = req.body.identifiantnom;
-        let prenom = req.body.identifiantprenom;
-        let email = req.body.identifiantmail;
-        let naissance = req.body.identifiantnaissance;
-        let mdp = req.body.mdp;
-        let requeteSQL;
-        let donnees;
-    
+            console.log("corps requête body", req.body);
+            console.log("corps requête nom", req.body.identifiantnom);
+            console.log("corps requête prénom", req.body.identifiantprenom);
+            console.log("corps requête de l'email", req.body.identifiantmail);
+            console.log("corps requête date de naissance", req.body.identifiantnaissance);
+            console.log("corps requête mot de passe", req.body.mdp);
         
+            
+            let nom = req.body.identifiantnom;
+            let prenom = req.body.identifiantprenom;
+            let email = req.body.identifiantmail;
+            let naissance = req.body.identifiantnaissance;
+            let mdp = req.body.mdp;
+            
+            
         
-        requeteSQL = "INSERT INTO utilisateur (id, nom, prenom, email, mot_de_passe, date_de_naissance) VALUES (?, ?, ?, ?, ?, ? )";
-        donnees = [null, nom, prenom, email, mdp, naissance];
-    
-        req.getConnection((erreur, connection) => {
-            if (erreur) {
-                console.log("erreur");
-            } else {
-                
-                connection.query(requeteSQL, donnees, (err, ajout) => {
-                    if (err) {
-                        console.log("erreur de de connexion", err);
+            // utilisation de bcrypt pour hacher le mot de passe 
+            try {
+                const hashedPassword = await bcrypt.hash(mdp, saltRounds);
+
+                let requeteSQL = "INSERT INTO utilisateur (id, nom, prenom, email, mot_de_passe, date_de_naissance) VALUES (?, ?, ?, ?, ?, ? )";
+                let donnees = [null, nom, prenom, email, hashedPassword, naissance];
+        
+                req.getConnection((erreur, connection) => {
+                    if (erreur) {
+                        console.log("erreur", erreur);
                     } else {
-                        console.log("mission réussi");
-                        res.status(302).redirect("/signup");
+                        connection.query(requeteSQL, donnees, (err, ajout) => {
+                            if (err) {
+                            console.log("erreur de de connexion", err);
+                            } else {
+                            console.log("mission réussi");
+                            res.status(302).redirect("/signup");
+                            }
+                        });
                     }
                 });
-            }
-        });
+            } catch (err) {
+            console.log("erreur lors du hachage de mot de passe", err);
+            res.status(500).send("erreur interne du serveur")
+             }
     }
-}
+        
+            
+};
